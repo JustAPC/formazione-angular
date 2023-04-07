99b40ce5f14d64a7c3d784c4c13dc7c8ee238f32
JustAPC <andreapontillo.dev@gmail.com>
2023-04-07 12:27:30 +0200

    validation on decease date earlier than birth date

--- src/app/components/person-form/person-form.component.ts
+++ src/app/components/person-form/person-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Validators, FormBuilder, FormGroup } from "@angular/forms";
+import { Validators, FormBuilder, FormGroup, ValidatorFn, AbstractControl } from "@angular/forms";
 
 @Component({
   selector: "app-person-form",
@@ -9,6 +9,7 @@ import { Validators, FormBuilder, FormGroup } from "@angular/forms";
 export class PersonFormComponent implements OnInit {
   personForm!: FormGroup;
   today: Date = new Date();
+  birthdate!: Date;
   taxcodeValidation = "^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$";
 
   constructor(private fb: FormBuilder) {}
@@ -17,14 +18,50 @@ export class PersonFormComponent implements OnInit {
     this.personForm = this.fb.group({
       firstname: ["", [Validators.required, Validators.minLength(2)]],
       lastname: ["", [Validators.required, Validators.minLength(2)]],
-      birthdate: ["", [Validators.required]],
-      deceasedate: [""],
+      birthdate: ["", [Validators.required, this.birthDateValidator()]],
+      deceasedate: ["", this.deceaseDateValidator()],
       taxcode: ["", [Validators.required, Validators.pattern(this.taxcodeValidation)]],
       gender: ["", Validators.required],
     });
   }
 
+  private birthDateValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const today = new Date().getTime();
+      if (!(control && control.value)) {
+        // if there's no control or no value, that's ok
+        return null;
+      }
+      // return null if there's no errors
+      this.birthdate = control.value;
+      return control.value.getTime() > today
+        ? { invalidDate: "You cannot use future dates" }
+        : null;
+    };
+  }
+
+  private deceaseDateValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      if (!(control && control.value)) {
+        // if there's no control or no value, that's ok
+        return null;
+      }
+      // return null if there's no errors
+      if (this.birthdate != null) {
+        return control.value.getTime() < this.birthdate.getTime() ? { invalidDate: true } : null;
+      }
+
+      return null;
+    };
+  }
+
   onSubmit() {
+    if (
+      this.personForm.controls["birthdate"].value > this.personForm.controls["deceasedate"].value
+    ) {
+      return;
+    }
+
     console.log(JSON.stringify(this.personForm.value, null, 4));
 
     if (this.personForm.status === "VALID") {
